5345e2f39a0749e1a90455c888291b0bcc7bc5e1
You Jin Kim <youjinkim@Yous-MacBook-Pro.local>
2023-05-05 01:25:09 -0700

    Change Date display format. Remove debug codes.

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
+// Dictionary is used to store data
 var myDay = {};
 
-
 $(function () {
-  // Generate Time Blocks
+  // Generate Time Blocks (ensure all DOM are rendered before the next steps)
   // Load any previous data from LocalStorage
   // Show the descriptions to the corresponding textarea elements.
   init();
@@ -14,98 +14,74 @@ $(function () {
   // Display the current date in the header of the page.
   getHeaderDate();
 
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // Listener for click events on the save button.
   $(".saveBtn").on("click", function() {
-    
     var textValue = $(this).siblings('.description').val();
     var timeIndex = $(this).parent().attr('id')
-
     myDay[timeIndex] = textValue;
-
     saveReminders();
-
   } );
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
+  // Color code the block by the hour
   $(".time-block").each( function () {
-      var currentHour = dayjs().format("HH");
-      var blockHour = $(this).attr('id').split('-')[1];
-      
-      currentHour = parseInt(currentHour);
-      blockHour = parseInt(blockHour);
-      
-      currentHour += 10;  // for test purpose
+    // hours are in 24 format
+    var currentHour = dayjs().format("HH");
+    var blockHour = $(this).attr('id').split('-')[1];
+    
+    currentHour = parseInt(currentHour);
+    blockHour = parseInt(blockHour);
+    
+    // currentHour += 12;  // for test purpose
 
-      $(this).removeClass('future')
-      $(this).removeClass('past')
-      $(this).removeClass('present')
+    // clear all color-coding classes
+    $(this).removeClass('future')
+    $(this).removeClass('past')
+    $(this).removeClass('present')
 
-      if( currentHour < blockHour ) {
-        $(this).addClass('future')
-      } else if(currentHour === blockHour) {
-        $(this).addClass('present')
-      } else {
-        $(this).addClass('past')
-      }
+    // add clss for color coding according to the hour
+    if( currentHour < blockHour ) {
+      $(this).addClass('future')
+    } else if(currentHour === blockHour) {
+      $(this).addClass('present')
+    } else {
+      $(this).addClass('past')
+    }
   } );
 
 });
 
-
+// display current date
 function getHeaderDate() {
-  var currentHeaderDate = dayjs().format('dddd, MMMM Do');
+  var currentHeaderDate = dayjs().format('MMMM D. YYYY (dddd)');
   $("#currentDay").text(currentHeaderDate);
 }
-
 
 // saves data to localStorage
 function saveReminders() {
   localStorage.setItem("myDay", JSON.stringify(myDay));
-  // console.log(JSON.stringify(myDay));
 }
 
 // sets any data in localStorage to the view
 function displayReminders() {
-  // myDay.forEach(function (_thisHour) {
-  //     $(`#${_thisHour.id}`).val(_thisHour.reminder);
-  // })
   for( var hourId in myDay) {
     $(`#${hourId}`).children('.description').val(myDay[hourId]);
   }
 }
 
-// sets any existing localStorage data to the view if it exists
+// Generate TimeSlots and sets any existing localStorage data to the view if it exists
 function init() {
-
-  createTimeSlots();
-
+  generateTimeSlots();
   var storedDay = JSON.parse(localStorage.getItem("myDay"));
-
   if (storedDay) {
       myDay = storedDay;
-      // console.log(myDay);
-      // displayReminders only if previous data exists
       displayReminders();
   }
-  // saveReminders();
 }
 
-function createTimeSlots() {
-  
+function generateTimeSlots() {
   var timeBlocksStr = "";
 
-  // for(var hour24=0;hour24<24;hour24++) {
+  // iterate from 9am to 5pm
   for(var hour24=9;hour24<=17;hour24++) {
     hour = hour24 % 12;
     if (hour==0) hour=12;
@@ -123,9 +99,5 @@ function createTimeSlots() {
       </div>
     `
   }
-  // console.log(timeBlocksStr);
-
   $('.container-lg').html(timeBlocksStr);
-  
-
 }
